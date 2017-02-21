8b1432035dbde3936f7d752caba17a142633224c
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-21 12:02:34 +0300

    PartialView Nedir

---
+++ 05_PartialViewSamplw/Controllers/Home2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _05_PartialViewSamplw.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home2
+        public ActionResult page1()
+        {
+            return View();
+        }
+    }
+}
